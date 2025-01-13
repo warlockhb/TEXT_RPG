54e816c66058582b029a15b967f95e6c55ce0550
Cabbage <49223403+healthcabbage@users.noreply.github.com>
2025-01-13 17:35:36 +0900

    오타수정&인벤토리주석
    - 급한 오타 부분 수정과 인벤토리 부분을 잠시 주석 처리해두었습니다.
    - 아이템 코드 참고후 다시 주석해제 및 수정을 진행하겠습니다.

--- TEXT_RPG/TEXT_RPG/Character.h
+++ TEXT_RPG/TEXT_RPG/Character.h
@@ -4,8 +4,8 @@
 #include <vector>
 using namespace std;
 
-class PassiveItem;
-class Equipment;
+//class PassiveItem;
+//class Equipment;
 
 class Character
 {
@@ -23,11 +23,11 @@ private:
 	int Gold;
 
 	//인벤토리
-	vector<PassiveItem*> Inventory;
-	size_t Max_Inventory_Size = 7;
+	//vector<PassiveItem*> Inventory;
+	//size_t Max_Inventory_Size = 7;
 	//장비칸
-	vector<Equipment*> Equipment_Inventory;
-	const size_t Max_Skill_Size = 5;
+	//vector<Equipment*> Equipment_Inventory;
+	//const size_t Max_Skill_Size = 5;
 
 	Character(string New_name);
 
@@ -36,7 +36,7 @@ private:
 
 public:
 	//싱글톤
-	static Character * GetInstance(string name = "");
+	static Character * GetInstance(string New_name = "");
 
 	//플레이어스테이트보여주기
 	void DisPlayStatus();
@@ -45,17 +45,17 @@ public:
 	void LevelUp();
 
 	//아이템사용&추가&제거
-	void UseItem(int index);
-	void AddItem(PassiveItem* item);
-	void RemoveItem(PassiveItem* item);
+	//void UseItem(int index);
+	//void AddItem(PassiveItem* item);
+	//void RemoveItem(PassiveItem* item);
 
 	//스킬사용&추가&제거
-	void SkillUse(int index);
-	void AddSkill(Equipment* skill);
-	void RemoveSkill(Equipment* skill);
+	//void SkillUse(int index);
+	//void AddSkill(Equipment* skill);
+	//void RemoveSkill(Equipment* skill);
 
 	//인벤토리 추가
-	void ByeInventory(int buycount);
+	//void ByeInventory(int buycount);
 
 	//플레이어 사망
 	void Die();
