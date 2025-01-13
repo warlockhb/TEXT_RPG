ca74b94b68fdff74cb25a0f9b501252a7f66bacd
Cabbage <49223403+healthcabbage@users.noreply.github.com>
2025-01-13 16:04:56 +0900

    캐릭터 중복 코드 수정

--- TEXT_RPG/TEXT_RPG/Character.cpp
+++ TEXT_RPG/TEXT_RPG/Character.cpp
@@ -1,1 +1,204 @@
-﻿
+﻿#include "Character.h"
+em#include "Character.h"
+
+Character * Character::instance = nullptr;
+
+Character::Character(string New_name)
+{
+	this->Name = New_name;
+	this->Level = 1;
+	this->MaxHealth = 100;
+	this->Health = MaxHealth;
+	this->Attack = 30;
+	this->Exp = 0;
+	this->MaxExp = 100;
+	this->Gold = 0;
+	this->Inventory.reserve(Max_Inventory_Size);
+	this->Equipment_Inventory.reserve(Max_Skill_Size);
+}
+
+Character* Character::GetInstance(string name)
+{
+	if (instance == nullptr)
+	{
+		instance = new Character();
+	}
+
+	return instance;
+}
+
+
+void Character::DisPlayStatus()
+{
+	cout << "=============================" << endl;
+	cout << "이름 : " << Name << endl;
+	cout << "레벨 : " << Level << endl;
+	cout << "체력 : " << Health << "/" << MaxHealth << endl;
+	cout << "공격력 : " << Attack << endl;
+	cout << "현재 경험치 : " << Exp << endl;
+	cout << "다음 레벨까지의 경험치 : " << MaxExp - Exp << endl;
+	cout << "보유 골드 : " << Gold << endl;
+}
+
+
+void Character::LevelUp()
+{
+	if (Level < 10 && exp >= maxExp)
+	{
+		this->Level++;
+		this->MaxHealth += Level * 20;
+		this->Health = MaxHealth;
+		this->Attack += Level * 5;
+		this->Exp = 0;
+		cout << "레벨 업!" << endl;
+	}
+	if (Level >= 10)
+	{
+		cout << "최대 레벨!" << endl;
+	}
+}
+
+void Character::UseItem(int index)
+{
+	if (index < 0 || index >= Inventory.size())
+	{
+		cout << "잘못 입력되었습니다." << endl;
+	}
+
+	PassiveItem* item = Inventory[index];
+	//아이템 사용
+	//item->use(this)?
+
+	delete item;
+	Inventory.erase(Inventory.begin() + index);
+	cout << "아이템을 사용했습니다!" << endl;
+}
+
+void Character::AddItem(PassiveItem* item)
+{
+	if (Inventory.size() < Max_Inventory_Size)
+	{
+		Inventory.push_back(item);
+		cout << "인벤토리에 아이템이 추가되었습니다." << endl;
+	}
+	else
+	{
+		cout << "인벤토리가 가득차서, 아이템을 추가할 수 없습니다." << endl;
+	}
+}
+
+void Character::RemoveItem(PassiveItem* item)
+{
+	auto it = find(Inventory.begin(), Inventory.end(), item);
+
+	if (it != Inventory.end())
+	{
+		Inventory.erase(it);
+	}
+}
+
+void Character::SkillUse(int index)
+{
+	if (index < 0 || index >= Equipment_Inventory.size())
+	{
+		cout << "잘못 입력되었습니다." << endl;
+	}
+
+	Equipment* skill = Equipment_Inventory[index];
+
+	delete skill;
+	cout << "스킬을 사용했습니다!" << endl;
+}
+
+void Character::AddSkill(Equipment* skill)
+{
+	if (Equipment_Inventory.size() < Max_Skill_Size)
+	{
+		Equipment_Inventory.push_back(skill);
+		cout << "스킬이 추가되었습니다." << endl;
+	}
+	else
+	{
+		cout << "인벤토리가 가득차서, 스킬을 추가할 수 없습니다." << endl;
+	}
+}
+
+void Character::RemoveSkill(Equipment* skill)
+{
+	auto it = find(Equipment_Inventory.begin(), Equipment_Inventory.end(), skill);
+
+	if (it != Equipment_Inventory.end())
+	{
+		Equipment_Inventory.erase(it);
+	}
+}
+
+void Character::ByeInventory(int buycount)
+{
+	switch (buycount)
+	{
+	case 1:
+		Max_Inventory_Size += 3;
+		Inventory.reserve(Max_Inventory_Size);
+		cout << "인벤토리를 확장되었습니다. 현재 인벤토리 사이즈 : " << Inventory.size() << endl;
+		break;
+	case 2:
+		Max_Inventory_Size += 5;
+		Inventory.reserve(Max_Inventory_Size);
+		cout << "인벤토리를 확장되었습니다. 현재 인벤토리 사이즈 : " << Inventory.size() << endl;
+		break;
+	default:
+		cout << "인벤토리를 최대 확장했습니다!" << endl;
+		break;
+	}
+}
+
+int Character::Getgold()
+{
+	return Gold;
+}
+
+void Character::MinusGold(int buymoney)
+{
+	if (Gold >= buymoney)
+	{
+		Gold -= buymoney;
+		cout << "골드를 사용했습니다! 사용된 골드 : " << buymoney << "남은 골드 : " << Gold << endl;
+	}
+	else
+	{
+		cout << "골드가 부족합니다! 현재 보유 골드 : " << Gold << endl;
+	}
+}
+
+void Character::PlusGold(int sellmoney)
+{
+	Gold += sellmoney;
+	cout << "현재 보유 골드 : " << Gold << endl;
+}
+
+void Character::SetExp(int plusexp)
+{
+	if (Level < 10)
+	{
+		if (Exp < MaxExp)
+		{
+			Exp += plusexp;
+		}
+		else
+		{
+			LevelUp();
+			Exp += plusexp;
+		}
+	}
+}
+
+void Character::GetAttack()
+{
+	return Attack;
+}
+
+void Character::GetHealth()
+{
+	return Health;
+}
--- TEXT_RPG/TEXT_RPG/Character.h
+++ TEXT_RPG/TEXT_RPG/Character.h
@@ -1,1 +1,72 @@
-﻿
+﻿#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class PassiveItem;
+class Equipment;
+
+class Character
+{
+private:
+	static Character * instance;
+
+	//수치 파라미터
+	string Name;
+	int Level;
+	int Health;
+	int MaxHealth;
+	int Attack;
+	int Exp;
+	int MaxExp;
+	int Gold;
+
+	//인벤토리
+	vector<PassiveItem*> Inventory;
+	size_t Max_Inventory_Size = 7;
+	//장비칸
+	vector<Equipment*> Equipment_Inventory;
+	const size_t Max_Skill_Size = 5;
+
+	Character(string New_name);
+
+	Character(const Chracter&) = delete;
+	Character& operator = (const Character&) = delete;
+
+public:
+	//싱글톤
+	static Character * GetInstance(string name = "");
+
+	//플레이어 스테이트 보여주기
+	void DisPlayStatus();
+
+	void LevelUp();
+
+	void UseItem(int index);
+
+	void AddItem(PassiveItem* item);
+
+	void RemoveItem(PassiveItem* item);
+
+	void SkillUse(int index);
+
+	void AddSkill(Equipment* skill);
+
+	void RemoveSkill(Equipment* skill);
+
+	void ByeInventory(int buycount);
+
+	void MinusGold(int buymoney);
+
+	void PlusGold(int sellmoney);
+
+	//Get
+	int GetAttack();
+	int GetHealth();
+	int Getgold();
+
+	//Set
+	void SetExp(int plusexp);
+};
+
