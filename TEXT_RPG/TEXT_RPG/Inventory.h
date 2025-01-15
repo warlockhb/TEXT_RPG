1de919a1847db943d22b9509c9dc21c32299e78d
HBpencil <thdnfla234@gmail.com>
2025-01-15 12:37:40 +0900

    구조체 Stat 분리

--- TEXT_RPG/TEXT_RPG/Inventory.h
+++ TEXT_RPG/TEXT_RPG/Inventory.h
@@ -3,19 +3,10 @@
 #include <vector>
 #include "Item/PassiveItem/PassiveItem.h"
 #include "Character.h"
+#include "Stat.h"
+
 using namespace std;
 
-
-struct Stat
-{
-	int Level = 0;
-	int Health = 0;
-	int MaxHealth = 0;
-	int Attack = 0;
-	int Exp = 0;
-	int MaxExp = 0;
-	int Gold = 0;
-};
 
 
 class PassiveItem;
---
+++ TEXT_RPG/TEXT_RPG/Stat.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+struct Stat
+{
+    int Level = 0;
+    int Health = 0;
+    int MaxHealth = 0;
+    int Attack = 0;
+    int Exp = 0;
+    int MaxExp = 0;
+    int Gold = 0;
+};
